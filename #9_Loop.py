3346612d45f4348f1ff62cfa401cec17aabc03d7
CodeXplore <codexplore.channel@gmail.com>
2020-09-20 22:35:46 +0800

    Update #9_Loop.py

--- #9_Loop.py
+++ #9_Loop.py
@@ -1,4 +1,29 @@
-msg = ''
-while msg != 'quit':
-    msg = input("Please give me an input: ")
-    print(msg)
+#LOOP: For & While
+
+my_list = [1,2,3]
+my_tuple = (1,2,3)
+my_list2 = [(1,2), (3,4), (5,6)]
+my_dict = {'a': 1, 'b': 2. 'c': 3}
+
+for num in my_list:
+    print(num) # 1, 2, 3
+
+for num in my_tuple:
+    print(num) # 1, 2, 3
+
+for num in my_list2:
+    print(num) # (1,2), (3,4), (5,6)
+
+for num in '123':
+    print(num) # 1, 2, 3
+
+for k,v in my_dict.items(): #Dictionary Unpacking
+    print(k) # 'a', 'b', 'c'
+    print(v) # 1, 2, 3
+
+while <condition that evaluates to boolean>:
+  # action
+  if <condition that evaluates to boolean>:
+    break # break out of while loop
+  if <condition that evaluates to boolean>:
+    continue #continue to the next line in the block
