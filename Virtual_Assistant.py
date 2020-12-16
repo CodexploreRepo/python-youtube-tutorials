382a653e279930b979bdc0913015d31a5c73507f
CodeXplore <codexplore.channel@gmail.com>
2020-12-16 22:22:46 +0800

    Create Virtual_Assistant.py

---
+++ Virtual_Assistant.py
@@ -0,0 +1,76 @@
+import pyttsx3
+import datetime 
+import speech_recognition as sr
+import webbrowser as wb
+import os
+
+
+friday=pyttsx3.init()
+voices = friday.getProperty('voices')
+friday.setProperty('voice', voices[1].id) 
+
+def speak(audio):
+    print('F.R.I.D.A.Y: ' + audio)
+    friday.say(audio)
+    friday.runAndWait()
+   
+    
+def time():
+    Time=datetime.datetime.now().strftime("%I:%M:%p") 
+    speak("It is")
+    speak(Time)
+
+def welcome():
+        #Chao hoi
+        hour=datetime.datetime.now().hour
+        if hour >= 6 and hour<12:
+            speak("Good Morning Sir!")
+        elif hour>=12 and hour<18:
+            speak("Good Afternoon Sir!")
+        elif hour>=18 and hour<24:
+            speak("Good Evening sir")
+        speak("How can I help you,boss") 
+
+
+def command():
+    c=sr.Recognizer()
+    with sr.Microphone() as source:
+        c.pause_threshold=2
+        audio=c.listen(source)
+    try:
+        query = c.recognize_google(audio,language='en-US')
+        print("Tony Lèo: "+query)
+    except sr.UnknownValueError:
+        print('Sorry sir! I didn\'t get that! Try typing the command!')
+        query = str(input('Your order is: '))
+    return query
+
+if __name__  =="__main__":
+    welcome()
+
+    while True:
+        query=command().lower()
+        #All the command will store in lower case for easy recognition
+        if "google" in query:
+            speak("What should I search,boss")
+            search=command().lower()
+            url = f"https://google.com/search?q={search}"
+            wb.get().open(url)
+            speak(f'Here is your {search} on google')
+        
+        elif "youtube" in query:
+            speak("What should I search,boss")
+            search=command().lower()
+            url = f"https://youtube.com/search?q={search}"
+            wb.get().open(url)
+            speak(f'Here is your {search} on youtube')
+
+        elif "quit" in query:
+            speak("Friday is off. Goodbye boss")
+            quit()
+        elif "open video" in query:
+            meme =r"C:\Users\Admin\Desktop\test\meme.mp4"
+            os.startfile(meme)
+        elif 'time' in query:
+            time()
+            
